8f14abe268476805ae254aa67d9bc6aeb5f3e0fd
dmytroNikanorov <dimonnikanorov2@gmail.com>
2023-02-02 23:41:49 +0200

    Import user module

--- src/companies/companies.module.ts
+++ src/companies/companies.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { User } from 'src/users/users.model';
+import { UsersModule } from 'src/users/users.module';
 import { CompaniesController } from './companies.controller';
 import { Company } from './companies.model';
 import { CompaniesService } from './companies.service';
@@ -8,6 +9,6 @@ import { CompaniesService } from './companies.service';
 @Module({
   controllers: [CompaniesController],
   providers: [CompaniesService],
-  imports: [SequelizeModule.forFeature([Company, User])]
+  imports: [SequelizeModule.forFeature([Company, User]), UsersModule]
 })
 export class CompaniesModule {}
