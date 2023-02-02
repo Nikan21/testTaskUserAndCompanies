98392ce5bd9261b9dba1f9336f1952192030c2ee
dmytroNikanorov <dimonnikanorov2@gmail.com>
2023-02-02 19:11:57 +0200

    Create company table

---
+++ src/companies/companies.model.ts
@@ -0,0 +1,42 @@
+import { Model, Column, DataType, Table, ForeignKey, BelongsTo } from "sequelize-typescript";
+import { User } from "src/users/users.model";
+
+interface CompanyCreationAttrs {
+    name: string;
+    address: string;
+    serviceOfActivity: string;
+    numberOfEmployess: number;
+    description: string;
+    type: string;
+}
+
+@Table({tableName: 'companies'})
+export class Company extends Model<Company, CompanyCreationAttrs> {
+
+    @Column({type: DataType.INTEGER, unique: true, autoIncrement: true, primaryKey: true})
+    id: number
+
+    @Column({type: DataType.STRING, unique: true, allowNull: true})
+    name: string;
+
+    @Column({type: DataType.STRING, allowNull: true})
+    address: string;
+
+    @Column({type: DataType.STRING, allowNull: true})
+    serviceOfActivity: string;
+
+    @Column({type: DataType.INTEGER, allowNull: true})
+    numberOfEmployess: number;
+
+    @Column({type: DataType.STRING, allowNull: true})
+    description: string;
+
+    @Column({type: DataType.STRING, allowNull: true})
+    type: string;
+
+/*     @ForeignKey(() => User)
+    @Column
+    userId: number;
+    @BelongsTo(() => User)
+    user: User; */
+}
