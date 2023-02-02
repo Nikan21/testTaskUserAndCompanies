8726aacf791831583284b5254d4a156968e0c93d
dmytroNikanorov <dimonnikanorov2@gmail.com>
2023-02-03 00:40:23 +0200

    Create get, patch, delete method for companyById

--- src/companies/companies.controller.ts
+++ src/companies/companies.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Get, Header, Post, Req } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Req } from '@nestjs/common';
 import { Request } from 'express';
 import { UsersService } from 'src/users/users.service';
 import { CompaniesService } from './companies.service';
 import { CreateCompanyDto } from './dto/create-company.dto';
+import { UpdateCompanyDto } from './dto/update-company.dto';
 
 @Controller()
 export class CompaniesController {
@@ -10,6 +11,7 @@ export class CompaniesController {
 
     @Get('companies')
     async getAll(@Req() req: Request) {
+        console.log(req.headers)
         const id = await this.usersService.getCurrentUserId(req)
         return this.companiesService.getAllCompanies(id)
     }
@@ -18,5 +20,20 @@ export class CompaniesController {
     async create(@Body() createCompanyDto: CreateCompanyDto, @Req() req: Request) {
         const id = await this.usersService.getCurrentUserId(req)
         return this.companiesService.createCompany(createCompanyDto, id)
+    }
+
+    @Get('company/:id')
+    get(@Param('id') id) {
+        return this.companiesService.getCompanyById(id)
+    }
+
+    @Patch('company/:id')
+    update(@Param('id') id, @Body() updateCompanyDto: UpdateCompanyDto) {
+        return this.companiesService.updateCompanyById(id, updateCompanyDto)
+    }
+
+    @Delete('company/:id')
+    remove(@Param('id') id) {
+        return this.companiesService.removeCompanyById(id)
     }
 }
--- src/companies/companies.service.ts
+++ src/companies/companies.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 import { Company } from './companies.model';
 import { CreateCompanyDto } from './dto/create-company.dto';
+import { UpdateCompanyDto } from './dto/update-company.dto';
 
 @Injectable()
 export class CompaniesService {
@@ -15,5 +16,24 @@ export class CompaniesService {
     async createCompany(createCompanyDto: CreateCompanyDto, id) {
         const company = await this.companyRepository.create({...createCompanyDto, userId: id})
         return company;
+    }
+
+    async getCompanyById(id) {
+        const company = await this.companyRepository.findOne({where: {id}})
+        return company;
+    }
+
+    async updateCompanyById(id, updateCompanyDto: UpdateCompanyDto) {
+        const company = await this.companyRepository.update(
+            {...updateCompanyDto},
+            {where: {id}, returning: true} 
+        )
+        return company
+    }
+
+    async removeCompanyById(id) {
+        const company = await this.getCompanyById(id)
+        company.destroy()
+        return company
     }
 }
