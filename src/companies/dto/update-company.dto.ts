970989aa2e4ca95391cb5e21e4b6807b7396ade5
dmytroNikanorov <dimonnikanorov2@gmail.com>
2023-02-03 00:39:45 +0200

    Create UpdateCompanyDto

---
+++ src/companies/dto/update-company.dto.ts
@@ -0,0 +1,8 @@
+export class UpdateCompanyDto {
+    readonly name: string;
+    readonly address: string;
+    readonly serviceOfActivity: string;
+    readonly numberOfEmployess: number;
+    readonly description: string;
+    readonly type: string;
+} 
