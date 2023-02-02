f47741c53cd3adebe817a9e09b9e8d9d86c96ddd
dmytroNikanorov <dimonnikanorov2@gmail.com>
2023-02-02 15:02:53 +0200

    Create registration and login endpoint

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Header, HttpCode, HttpStatus, Post, UseGuards } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { LoginUserDto } from './dto/login-user.dto';
+import { JwtAuthGuard } from './jwt-auth.guard';
+import { UsersService } from './users.service';
 
-@Controller('users')
-export class UsersController {}
+@Controller()
+export class UsersController {
+    constructor(private readonly usersService: UsersService) {}
+
+    @Post('/signin')
+    @Header('Content-Type', 'application/json')
+    login(@Body() loginUserDto: LoginUserDto) {
+        return this.usersService.loginUser(loginUserDto)
+    }
+
+    @Post('/signup')
+    @HttpCode(HttpStatus.CREATED)
+    @Header('Content-Type', 'application/json')
+    registration(@Body() createUserDto: CreateUserDto) {
+        return this.usersService.registrateUser(createUserDto)
+    }
+}
