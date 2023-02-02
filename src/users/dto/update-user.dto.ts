15eadfc4e673aec723efa0ac8ea823993eaeca4c
dmytroNikanorov <dimonnikanorov2@gmail.com>
2023-02-02 16:09:58 +0200

    Create update user dto

---
+++ src/users/dto/update-user.dto.ts
@@ -0,0 +1,10 @@
+export class UpdateUserDto {
+    firstName: string;
+    lastName: string;
+    nickname: string;
+    phoneNumber: number;
+    position: string;
+    description: string;
+    email: string;
+    password: string;
+}
