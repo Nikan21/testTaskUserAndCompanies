866a10c9e0116893aa03da9eb8ad8b92dd5a6469
dmytroNikanorov <dimonnikanorov2@gmail.com>
2023-02-02 13:10:45 +0200

    Create Users table

---
+++ src/users/users.model.ts
@@ -0,0 +1,44 @@
+import { Model } from "sequelize";
+import { Column, DataType, Table } from "sequelize-typescript";
+
+interface UserCreationAttrs {
+    firstName: string;
+    lastName: string;
+    nickname: string;
+    phoneNumber: number;
+    position: string;
+    description: string;
+    email: string;
+    password: string;
+}
+
+@Table({tableName: 'users'})
+export class User extends Model<User, UserCreationAttrs> {
+
+    @Column({type: DataType.INTEGER, unique: true, autoIncrement: true, primaryKey: true})
+    id: number
+
+    @Column({type: DataType.STRING, allowNull: true})
+    firstName: string;
+
+    @Column({type: DataType.STRING, allowNull: true})
+    lastName: string;
+
+    @Column({type: DataType.STRING, allowNull: true})
+    nickname: string;
+
+    @Column({type: DataType.INTEGER, unique: true, allowNull: true})
+    phoneNumber: number;
+
+    @Column({type: DataType.STRING, allowNull: true})
+    position: string;
+
+    @Column({type: DataType.STRING, allowNull: true})
+    description: string;
+
+    @Column({type: DataType.STRING, unique: true, allowNull: true})
+    email: string;
+
+    @Column({type: DataType.STRING, allowNull: true})
+    password: string;
+}
