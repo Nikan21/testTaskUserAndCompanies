3c67846d60eae59a842d981721f1e6834f3149e7
dmytroNikanorov <dimonnikanorov2@gmail.com>
2023-02-02 15:02:32 +0200

    Import Jwt library and add sign life token

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { UsersController } from './users.controller';
 import { User } from './users.model';
@@ -7,7 +8,12 @@ import { UsersService } from './users.service';
 @Module({
   controllers: [UsersController],
   providers: [UsersService],
-  imports: [SequelizeModule.forFeature([User])]
+  imports: [SequelizeModule.forFeature([User]), JwtModule.register({
+    secret: process.env.PRIVATE_Key || 'SECRET',
+    signOptions: {
+      expiresIn: '24h'
+    }
+  })]
 })
 
 export class UsersModule {}
