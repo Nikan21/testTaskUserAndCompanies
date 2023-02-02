a487e5c1e44323baaf09018e66bfe0f0c4e6c4ff
dmytroNikanorov <dimonnikanorov2@gmail.com>
2023-02-02 23:41:30 +0200

    Make good user function

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,21 +1,25 @@
-import { Body, Controller, Get, Param, Patch, UseGuards} from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Req, UseGuards} from '@nestjs/common';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { UsersService } from './users.service';
+import { Request } from 'express';
+
 
 @Controller('profile')
 export class UsersController {
     constructor(private readonly usersService: UsersService) {}
         
     @UseGuards(JwtAuthGuard)
-    @Get(':id')
-    getUser(@Param('id') id) {
+    @Get()
+    async getUser(@Req() req: Request) {
+        const id = await this.usersService.getCurrentUserId(req)
         return this.usersService.getUserById(id)
     }
      
     @UseGuards(JwtAuthGuard)
-    @Patch(':id')
-    updateUser(@Param('id') id, @Body() updateUserDto: UpdateUserDto) {
+    @Patch()
+    async updateUser(@Req() req: Request, @Body() updateUserDto: UpdateUserDto) {
+        const id = await this.usersService.getCurrentUserId(req)
         return this.usersService.updateUserById(id, updateUserDto)
     }
-}
+} 
