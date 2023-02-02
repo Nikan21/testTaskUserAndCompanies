f0503ae086b35b6b3f15e317034a3bc746608e4b
dmytroNikanorov <dimonnikanorov2@gmail.com>
2023-02-02 17:46:48 +0200

    Add guard on endpoint

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,16 +1,19 @@
-import { Body, Controller, Get, Param, Patch} from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, UseGuards} from '@nestjs/common';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { UsersService } from './users.service';
 
 @Controller('profile')
 export class UsersController {
     constructor(private readonly usersService: UsersService) {}
-
+        
+    @UseGuards(JwtAuthGuard)
     @Get(':id')
     getUser(@Param('id') id) {
         return this.usersService.getUserById(id)
     }
      
+    @UseGuards(JwtAuthGuard)
     @Patch(':id')
     updateUser(@Param('id') id, @Body() updateUserDto: UpdateUserDto) {
         return this.usersService.updateUserById(id, updateUserDto)
