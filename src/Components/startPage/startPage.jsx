c0ccbedc8cc98845aabb15b7454e7f1e3962796e
dmytroNikanorov <dimonnikanorov2@gmail.com>
2023-02-04 00:05:44 +0200

    Start create start page

---
+++ src/Components/startPage/startPage.jsx
@@ -0,0 +1,11 @@
+import { Fragment } from "react"
+import AuthHeader from "../Header/authHeader"
+
+
+export default function StartPage() {
+    return (
+        <Fragment>
+            <AuthHeader />
+        </Fragment>
+    )
+}
