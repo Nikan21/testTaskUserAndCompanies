2803533999b085f6f4181d107ff4517b3b8b14dc
dmytroNikanorov <dimonnikanorov2@gmail.com>
2023-02-04 00:03:16 +0200

    Add some route

--- src/index.js
+++ src/index.js
@@ -1,11 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import HandlerRouteError from './handlerRouterError'
+import FirstPage from './Components/fistPage/firstPage'
+import StartPage from './Components/startPage/startPage';
+import AnonymPage from './Components/anonymPage/anonymPage'
+import SignUpPage from './Components/signUpPage/signUpPage';
 import './index.css';
-import App from './App';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <FirstPage />,
+    errorElement: <HandlerRouteError />
+  },
+  {
+    path: '/main',
+    element: <StartPage />,
+    errorElement: <HandlerRouteError />
+  },
+  {
+    path: '/anonym',
+    element: <AnonymPage />,
+    errorElement: <HandlerRouteError />
+  },
+  {
+    path: '/signup',
+    element: <SignUpPage />,
+    errorElement: <HandlerRouteError />
+  },
+  {
+    path: '/signin',
+    element: <AnonymPage />,
+    errorElement: <HandlerRouteError />
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
