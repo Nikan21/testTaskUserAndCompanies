0114adb927b2d7d546471d9aaa6bf8586f22ec91
dmytroNikanorov <dimonnikanorov2@gmail.com>
2023-02-04 14:31:55 +0200

    Connect redux store in app

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import store from './store';
+import {Provider} from 'react-redux'
 import HandlerRouteError from './handlerRouterError'
 import FirstPage from './Components/fistPage/firstPage'
 import StartPage from './Components/startPage/startPage';
 import AnonymPage from './Components/anonymPage/anonymPage'
 import SignUpPage from './Components/signUpPage/signUpPage';
+import SignInPage from './Components/signInPage/signInPage';
 import './index.css';
 
 const router = createBrowserRouter([
@@ -31,7 +34,7 @@ const router = createBrowserRouter([
   },
   {
     path: '/signin',
-    element: <AnonymPage />,
+    element: <SignInPage />,
     errorElement: <HandlerRouteError />
   }
 ])
@@ -39,6 +42,8 @@ const router = createBrowserRouter([
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
     <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
