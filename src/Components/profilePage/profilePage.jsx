c248671c80daa50bd2467a784433dd8c44529432
dmytroNikanorov <dimonnikanorov2@gmail.com>
2023-02-08 20:31:26 +0200

    Change useEffect

--- src/Components/profilePage/profilePage.jsx
+++ src/Components/profilePage/profilePage.jsx
@@ -6,17 +6,16 @@ import GoodPage from './fulfilledPage'
 
 export default function ProfilePage() {
     const dispatch = useDispatch()
-    const currentToken = useSelector(state => state.token.value)
     const status = useSelector(state => state.getUser.status)
     const userData = useSelector(state => state.getUser.userData[0])
-    useEffect( () => {
-        if (status === 'idle') {
-            dispatch(getUserData(currentToken))
-        }
-    }, [dispatch, currentToken, status])
     console.log(userData)
 
-    
+    useEffect(() => {
+        if (status === 'idle') {
+            dispatch(getUserData())
+        }
+    }, [dispatch, status])
+
     return(
     <Fragment>
         {userData ? <GoodPage /> : <LoadingPage />}
