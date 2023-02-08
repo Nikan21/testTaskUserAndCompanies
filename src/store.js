256cbe6ab0db9c65fa0d90df2c321bd09224de74
dmytroNikanorov <dimonnikanorov2@gmail.com>
2023-02-08 20:31:07 +0200

    Delete token slice

--- src/store.js
+++ src/store.js
@@ -1,10 +1,8 @@
 import {configureStore} from '@reduxjs/toolkit'
-import tokenReducer from './slices/tokenSlice'
 import getUserReducer from './slices/getUserSlice'
 
 export default configureStore({
     reducer: {
-        token: tokenReducer,
         getUser: getUserReducer
     }
 })
