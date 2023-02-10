c9a1d091973bd68790dda069e0a95de8d6b51aca
dmytroNikanorov <dimonnikanorov2@gmail.com>
2023-02-10 19:39:27 +0200

    Add logout endpoint code

--- src/Components/Header/header.jsx
+++ src/Components/Header/header.jsx
@@ -1,13 +1,23 @@
-import {Link} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
+import {useDispatch} from 'react-redux'
+import { logOut } from '../../slices/userSlice'
 import styles from './header.module.sass'
 
 export default function Header() {
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+
+    const onLogOut = () => {
+        dispatch(logOut())
+        navigate('/anonym')
+    }
+
     return (
         <div className={styles.headerWrapper}>
             <Link className={styles.link} to='/profile'>Profile</Link>
             <Link className={styles.link} to='/companies'>Companies</Link>
             <Link className={styles.link} to='/createcompany'>Create company</Link>
-            <Link className={styles.link} to='/logout'>Log out</Link>
+            <Link className={styles.link} onClick={onLogOut} to='/anonym'>Log out</Link>
         </div>
     )
 }
