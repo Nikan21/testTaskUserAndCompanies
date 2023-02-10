19507fd17b4b217e3f90e3ee90a92c950ecd6ea6
dmytroNikanorov <dimonnikanorov2@gmail.com>
2023-02-10 18:42:12 +0200

    Rename file

---
+++ src/slices/userSlice.js
@@ -0,0 +1,66 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+export const getUserSlice = createSlice({
+    name: 'getUser',
+    initialState: {
+        userData: [],
+        updatedUserData: [],
+        statusGet: 'idle',
+        errorGet: null,
+        statusPatch: 'idle',
+        errorPatch: null,
+    },
+    reducers: {},
+    extraReducers(builder) {
+        builder
+            .addCase(getUserData.pending, (state) => {
+                state.statusGet = 'pending'
+            })
+            .addCase(getUserData.fulfilled, (state, action) => {
+                state.statusGet = 'fulfilled'
+                state.userData.shift()
+                state.userData.push(action.payload)
+            })
+            .addCase(getUserData.rejected, (state, action) => {
+                state.statusGet = 'rejected'
+                state.errorGet = action.error.message
+            })
+            .addCase(updateUserData.pending, (state) => {
+                state.statusPatch = 'pending'
+            })
+            .addCase(updateUserData.fulfilled, (state, action) => {
+                state.statusPatch = 'fulfilled'
+                state.updatedUserData.shift()
+                state.updatedUserData.push(action.payload)
+            })
+            .addCase(updateUserData.rejected, (state, action) => {
+                state.statusPatch = 'rejected'
+                state.errorPatch = action.error.message
+            })
+    }
+})
+
+export const getUserData = createAsyncThunk('get/user', async () => {
+    const response = await fetch('http://localhost:5000/profile', {
+        method: 'GET',
+        credentials: 'include',
+        mode: 'cors',
+        headers: {'Content-Type': 'application/json'},
+    })
+    const dataFromServer = await response.json()
+    return dataFromServer
+})
+
+export const updateUserData = createAsyncThunk('update/user', async (data) => {
+    const response = await fetch('http://localhost:5000/profile', {
+        method: 'PATCH',
+        credentials: 'include',
+        mode: 'cors',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(data)
+    })
+    const dataFromServer = await response.json()
+    return dataFromServer
+})
+
+export default getUserSlice.reducer
