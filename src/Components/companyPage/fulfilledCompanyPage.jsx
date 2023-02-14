2ceaad7cee0e4a39a1a4386e21fed21876dddbbc
dmytroNikanorov <dimonnikanorov2@gmail.com>
2023-02-14 17:40:26 +0200

    Add useEffect for change actualy data

--- src/Components/companyPage/fulfilledCompanyPage.jsx
+++ src/Components/companyPage/fulfilledCompanyPage.jsx
@@ -8,12 +8,19 @@ export default function FulfilledCompanyPage() {
     const dispatch = useDispatch()
     const status = useSelector(state => state.getCompanies.statusGet)
     const companiesData = useSelector(state => state.getCompanies.companiesData[0])
+    const updatedCompaniesData = useSelector(state => state.getCompanies.updatedCompaniesData[0])
 
     useEffect(() => {
         if (status === 'idle') {
             dispatch(getCompaniesData())
         }
     }, [dispatch, status])
+
+    useEffect(() => {
+        if (JSON.stringify(companiesData) !== JSON.stringify(updatedCompaniesData) && updatedCompaniesData) {
+            dispatch(getCompaniesData())
+        }
+    }, [dispatch, companiesData, updatedCompaniesData])
 
     return(
         <Fragment>
