7c339153f89aad66a1b93af45245b712dfc8b945
dmytroNikanorov <dimonnikanorov2@gmail.com>
2023-02-14 17:39:08 +0200

    Create patch and delete methods

--- src/slices/companiesSlice.js
+++ src/slices/companiesSlice.js
@@ -5,7 +5,12 @@ export const companiesSlice = createSlice({
     initialState: {
         companiesData: [],
         statusGet: 'idle',
-        errorGet: null
+        errorGet: null,
+        updatedCompaniesData: [],
+        statusPatch: 'idle',
+        errorPatch: null,
+        statusDelete: 'idle',
+        errorDelete: null
     },
     reducers: {},
     extraReducers(builder) {
@@ -22,12 +27,59 @@ export const companiesSlice = createSlice({
                 state.statusGet = 'rejected';
                 state.errorGet = action.error.message
             })
+            .addCase(updateCompanyData.pending, (state) => {
+                state.statusPatch = 'pending';
+            })
+            .addCase(updateCompanyData.fulfilled, (state, action) => {
+                state.statusPatch = 'fulfilled';
+                state.updatedCompaniesData.shift()
+                state.updatedCompaniesData.push(action.payload)
+            })
+            .addCase(updateCompanyData.rejected, (state, action) => {
+                state.statusPatch = 'rejected';
+                state.errorPatch = action.error.message
+            })
+            .addCase(removeCompany.pending, (state) => {
+                state.statusDelete = 'pending';
+            })
+            .addCase(removeCompany.fulfilled, (state, action) => {
+                state.statusDelete = 'fulfilled';
+                state.updatedCompaniesData.shift()
+                state.updatedCompaniesData.push(action.payload)
+            })
+            .addCase(removeCompany.rejected, (state, action) => {
+                state.statusDelete = 'rejected';
+                state.errorDelete = action.error.message
+            })
     }
 })
 
 export const getCompaniesData = createAsyncThunk('get/companies', async () => {
     const response = await fetch('http://localhost:5000/companies', {
         method: 'GET',
+        credentials: 'include',
+        mode: 'cors',
+        headers: {'Content-Type': 'application/json'},
+    })
+    const dataFromServer = await response.json()
+    return dataFromServer
+})
+
+export const updateCompanyData = createAsyncThunk('update/company', async(data) => {
+    const response = await fetch(`http://localhost:5000/company/${data[0]}`, {
+        method: 'PATCH',
+        credentials: 'include',
+        mode: 'cors',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(data[1])
+    })
+    const dataFromServer = await response.json()
+    return dataFromServer
+})
+
+export const removeCompany = createAsyncThunk('remove/company', async(id) => {
+    const response = await fetch(`http://localhost:5000/company/${id}`, {
+        method: 'DELETE',
         credentials: 'include',
         mode: 'cors',
         headers: {'Content-Type': 'application/json'},
