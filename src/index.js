7e6946eb5a5eaadf0d88406891d827973862f96e
dmytroNikanorov <dimonnikanorov2@gmail.com>
2023-02-14 17:38:45 +0200

    Add updateCompanyPage

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ import FulfilledProfileUpdatePage from './Components/profileUpdatePage/fulfilled
 import './index.css';
 import FulfilledCompaniesPage from './Components/companiesPage/fullfiledCompaniesPage';
 import FulfilledCompanyPage from './Components/companyPage/fulfilledCompanyPage';
+import FulfilledUpdateCompanyPage from './Components/updateCompanyPage/fulfilledUpdateCompanyPage';
 
 const router = createBrowserRouter([
   {
@@ -59,6 +60,11 @@ const router = createBrowserRouter([
   {
     path: '/company/:id',
     element: <FulfilledCompanyPage />,
+    errorElement: <HandlerRouteError />
+  },
+  {
+    path: '/company/update/:id',
+    element: <FulfilledUpdateCompanyPage />,
     errorElement: <HandlerRouteError />
   }
   
